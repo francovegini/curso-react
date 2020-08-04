6f25019ca762e31a1ba1a604063bbef896ea3249
Franco Vegini <francovegini@hotmail.com>
2020-08-04 16:19:54 -0300

    Props e componentes #2

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -9,12 +9,56 @@ const Bemvindo = (props) => {
     );
 }
 
+const Equipe = (props) => {
+    return (
+        <div>
+            <Sobre nome={props.nome}
+                   cargo={props.cargo}
+                   idade={props.idade}/>
+            <Social fb={props.facebook}
+                    yt={props.youtube}
+                    linkedin={props.linkedin}/>
+            <hr/>
+        </div>
+    );
+}
+
+const Sobre = (props) => {
+    return (
+        <div>
+            <h2>Olá, sou o {props.nome}</h2>
+            <h3>Cargo: {props.cargo}</h3>
+            <h3>Idade: {props.idade}</h3>
+        </div>
+    );
+}
+
+const Social = (props) => {
+    return (
+        <div>
+            <a href={props.fb}>Facebook </a>
+            <a href={props.linkedin}>LinkedIn </a>
+            <a href={props.yt}>YouTube </a>
+        </div>
+    );
+}
+
 function App() {
     return (
         <div>
-            Olá mundo!
-            <Bemvindo nome="Matheus" idade="19"/>
-            <Bemvindo nome="Pedro" idade="22"/>
+            <h1>Conheça nossa equipe:</h1>
+            <Equipe nome="Lucas"
+                    cargo="Programador"
+                    idade="20"
+                    facebook="http://www.facebook.com"
+                    linkedin="http://www.linkedin.com"
+                    youtube="http://www.youtube.com"/>
+            <Equipe nome="Lucas"
+                    cargo="Programador"
+                    idade="20"
+                    facebook="http://www.facebook.com"
+                    linkedin="http://www.linkedin.com"
+                    youtube="http://www.youtube.com"/>
         </div>
     )
 }
