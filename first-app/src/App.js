3ac600c214068d780ebfb724e412782a4bc82b07
Franco Vegini <francovegini@hotmail.com>
2020-08-04 17:01:19 -0300

    Aplicando class component

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,64 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Bemvindo = (props) => {
-    return (
-        <div>
-            <h2>Bem vindo(a) {props.nome}</h2>
-            <h3>Tenho {props.idade} anos</h3>
-        </div>
-    );
+class Equipe extends Component {
+    render() {
+        return (
+            <div>
+                <Sobre nome={this.props.nome}
+                       cargo={this.props.cargo}
+                       idade={this.props.idade}/>
+                <hr/>
+            </div>
+        );
+    }
 }
 
-const Equipe = (props) => {
-    return (
-        <div>
-            <Sobre nome={props.nome}
-                   cargo={props.cargo}
-                   idade={props.idade}/>
-            <Social fb={props.facebook}
-                    yt={props.youtube}
-                    linkedin={props.linkedin}/>
-            <hr/>
-        </div>
-    );
-}
-
-const Sobre = (props) => {
-    return (
-        <div>
-            <h2>Olá, sou o {props.nome}</h2>
-            <h3>Cargo: {props.cargo}</h3>
-            <h3>Idade: {props.idade}</h3>
-        </div>
-    );
-}
-
-const Social = (props) => {
-    return (
-        <div>
-            <a href={props.fb}>Facebook </a>
-            <a href={props.linkedin}>LinkedIn </a>
-            <a href={props.yt}>YouTube </a>
-        </div>
-    );
+class Sobre extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Olá, sou o {this.props.nome}</h2>
+                <h3>Cargo: {this.props.cargo}</h3>
+                <h3>Idade: {this.props.idade} anos</h3>
+            </div>
+        );
+    }
 }
 
 function App() {
     return (
         <div>
             <h1>Conheça nossa equipe:</h1>
-            <Equipe nome="Lucas"
+            <Equipe nome="Pedro"
                     cargo="Programador"
-                    idade="20"
-                    facebook="http://www.facebook.com"
+                    idade="19"
                     linkedin="http://www.linkedin.com"
-                    youtube="http://www.youtube.com"/>
+                    facebook="http://www.facebook.com"/>
             <Equipe nome="Lucas"
-                    cargo="Programador"
-                    idade="20"
-                    facebook="http://www.facebook.com"
-                    linkedin="http://www.linkedin.com"
-                    youtube="http://www.youtube.com"/>
+                    cargo="Designer"
+                    idade="21"/>
         </div>
     )
 }
---
+++ first-app/src/App2.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+const Bemvindo = (props) => {
+    return (
+        <div>
+            <h2>Bem vindo(a) {props.nome}</h2>
+            <h3>Tenho {props.idade} anos</h3>
+        </div>
+    );
+}
+
+const Equipe = (props) => {
+    return (
+        <div>
+            <Sobre nome={props.nome}
+                   cargo={props.cargo}
+                   idade={props.idade}/>
+            <Social fb={props.facebook}
+                    yt={props.youtube}
+                    linkedin={props.linkedin}/>
+            <hr/>
+        </div>
+    );
+}
+
+const Sobre = (props) => {
+    return (
+        <div>
+            <h2>Olá, sou o {props.nome}</h2>
+            <h3>Cargo: {props.cargo}</h3>
+            <h3>Idade: {props.idade}</h3>
+        </div>
+    );
+}
+
+const Social = (props) => {
+    return (
+        <div>
+            <a href={props.fb}>Facebook </a>
+            <a href={props.linkedin}>LinkedIn </a>
+            <a href={props.yt}>YouTube </a>
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <div>
+            <h1>Conheça nossa equipe:</h1>
+            <Equipe nome="Lucas"
+                    cargo="Programador"
+                    idade="20"
+                    facebook="http://www.facebook.com"
+                    linkedin="http://www.linkedin.com"
+                    youtube="http://www.youtube.com"/>
+            <Equipe nome="Lucas"
+                    cargo="Programador"
+                    idade="20"
+                    facebook="http://www.facebook.com"
+                    linkedin="http://www.linkedin.com"
+                    youtube="http://www.youtube.com"/>
+        </div>
+    )
+}
+
+export default App;
