8a6de03e5e8138f32a3bc6b89512156609fecc20
Franco Vegini <francovegini@hotmail.com>
2020-08-10 21:17:58 -0300

    Renderização condicional

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,12 +1,55 @@
 import React, {Component} from 'react';
-import Membro from "./components/Membro";
 
 class App extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            status: false
+        };
+
+        this.entrar = this.entrar.bind(this);
+        this.sair = this.sair.bind(this);
+    }
+
+    sair() {
+        this.setState({status: false})
+    }
+
+    entrar() {
+        this.setState({status: true})
+    }
 
     render() {
         return (
-            <Membro nome="Visitante"/>
+            <div>
+                {/*{this.state.status === 1 &&*/}
+                {/*    <h1>Bem-vindo ao sistema!</h1>*/}
+                {/*}*/}
+
+                {/*<div>*/}
+                {/*    <h2>Curso React JS</h2>*/}
+                {/*</div>*/}
+
+                {this.state.status ?
+                    <div>
+                        <h2> Bem-vindo ao sistema </h2>
+                        <button onClick={this.sair}>
+                            Sair do sistema
+                        </button>
+                    </div> :
+                    <div>
+                        <h2>Olá visitante, faça login!</h2>
+                        <button onClick={this.entrar}>
+                            Entrar no sistema
+                        </button>
+                    </div>
+                }
+                <hr/>
+
+
+
+            </div>
         );
     }
 }
---
+++ first-app/src/App6.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Membro from "./components/Membro";
+
+class App extends Component {
+
+
+    render() {
+        return (
+            <Membro nome="Visitante"/>
+        );
+    }
+}
+
+export default App;
