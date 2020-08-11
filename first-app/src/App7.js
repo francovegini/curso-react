797445f284f1d0982a0e918e86be94b5c42fef0f
Franco Vegini <francovegini@hotmail.com>
2020-08-10 21:20:38 -0300

    Ajuste para prox aula

---
+++ first-app/src/App7.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+
+/**
+ * Renderização condicional
+ */
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            status: false
+        };
+
+        this.entrar = this.entrar.bind(this);
+        this.sair = this.sair.bind(this);
+    }
+
+    sair() {
+        this.setState({status: false})
+    }
+
+    entrar() {
+        this.setState({status: true})
+    }
+
+    render() {
+        return (
+            <div>
+                {/*{this.state.status === 1 &&*/}
+                {/*    <h1>Bem-vindo ao sistema!</h1>*/}
+                {/*}*/}
+
+                {/*<div>*/}
+                {/*    <h2>Curso React JS</h2>*/}
+                {/*</div>*/}
+
+                {this.state.status ?
+                    <div>
+                        <h2> Bem-vindo ao sistema </h2>
+                        <button onClick={this.sair}>
+                            Sair do sistema
+                        </button>
+                    </div> :
+                    <div>
+                        <h2>Olá visitante, faça login!</h2>
+                        <button onClick={this.entrar}>
+                            Entrar no sistema
+                        </button>
+                    </div>
+                }
+            </div>
+        );
+    }
+}
+
+export default App;
