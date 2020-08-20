78a288428614d402af3b0d206ce87d74b7be0efb
Franco Vegini <francovegini@hotmail.com>
2020-08-20 19:03:06 -0300

    Manipulando formulários com react

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,39 +1,66 @@
 import React, {Component} from 'react';
-import Feed from "./components/Feed";
 
 /**
- * Trabalhando com listas.
+ * Manipulando formulários
  */
-class App8 extends Component {
+class App extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            feed: [
-                {id: 1, username: 'Matheus', curtidas: 10, comentarios: 2},
-                {id: 2, username: 'Lucas', curtidas: 147, comentarios: 31},
-                {id: 3, username: 'Marina', curtidas: 0, comentarios: 24},
-                {id: 4, username: 'Pietro', curtidas: 25, comentarios: 0}
-            ]
+            email: 'teste@teste.com',
+            password: '',
+            gender: 'MALE'
         };
 
+        this.changeEmail = this.changeEmail.bind(this);
+        this.changePassword = this.changePassword.bind(this);
+        this.changeGender = this.changeGender.bind(this);
+    }
+
+    changeEmail(e) {
+        let value = e.target.value;
+        this.setState({email: value});
+    }
+
+    changePassword(e) {
+        let value = e.target.value;
+        this.setState({password: value});
+    }
+
+    changeGender(e) {
+        let value = e.target.value;
+        this.setState({gender: value});
     }
 
     render() {
         return (
             <div>
-                {this.state.feed.map((item) => {
-                    return (
-                        <Feed id={item.id}
-                              username={item.username}
-                              curtidas={item.curtidas}
-                              comentarios={item.comentarios}/>
-                    )
-                })}
+                <h2>Login</h2>
+                Email:
+                <input type="email" name="email"
+                       value={this.state.email} onChange={this.changeEmail}/>
+                <br/>
+                Senha:
+                <input type="password" name="senha" value={this.state.password}
+                       onChange={this.changePassword}/>
+                <br/>
+                Gender:
+                <select name="gender" value={this.state.gender} onChange={this.changeGender}>
+                    <option value="MALE">Male</option>
+                    <option value="FEMALE">Female</option>
+                </select>
+
+                <hr/>
+                <div>
+                    <h3>{this.state.email}</h3>
+                    <h3>{this.state.password}</h3>
+                    <h3>{this.state.gender}</h3>
+                </div>
 
             </div>
         );
     }
 }
 
-export default App8;
+export default App;
