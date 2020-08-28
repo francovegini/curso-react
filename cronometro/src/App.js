3492b7d0ec62f61d5de023dd87dd6fa960c0a2cd
francovegini <francovegini@hotmail.com>
2020-08-27 23:44:34 -0300

    estilizado cronometro

--- cronometro/src/App.js
+++ cronometro/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import './style.css'
 
 class App extends Component {
     render() {
         return (
-            <div>
-
+            <div className="container">
+                <img src={ require('./assets/cronometro.png') } className="img"/>
+                <a className="timer">0.0</a>
+                <div className="areaBtn">
+                    <a className="botao">VAI</a>
+                    <a className="botao">ZERAR</a>
+                </div>
             </div>
         );
     }
