02cb8b651aff57b94fb951664d55a931a7ca0b08
francovegini <francovegini@hotmail.com>
2020-08-28 23:58:45 -0300

    Implementado metodo vai()

--- cronometro/src/App.js
+++ cronometro/src/App.js
@@ -2,14 +2,51 @@ import React, { Component } from 'react';
 import './style.css'
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            numero: 0,
+            botaoLabel: "VAI"
+        }
+
+        this.timer = null;
+        this.vai = this.vai.bind(this);
+        this.zerar = this.zerar.bind(this);
+    }
+
+    vai() {
+        let state = this.state
+
+        if (this.timer !== null) {
+            clearInterval(this.timer);
+            this.timer = null;
+            state.botaoLabel = "VAI";
+        } else {
+            this.timer = setInterval(() => {
+                let state = this.state;
+                state.numero += 0.1;
+                this.setState(state);
+            }, 100);
+            state.botaoLabel = "PAUSAR";
+        }
+
+        this.setState(state);
+    }
+
+    zerar() {
+
+    }
+
+
     render() {
         return (
             <div className="container">
                 <img src={ require('./assets/cronometro.png') } className="img"/>
-                <a className="timer">0.0</a>
+                <a className="timer">{ this.state.numero.toFixed(1) }</a>
                 <div className="areaBtn">
-                    <a className="botao">VAI</a>
-                    <a className="botao">ZERAR</a>
+                    <a className="botao" onClick={ this.vai }>{this.state.botaoLabel}</a>
+                    <a className="botao" onClick={ this.zerar }>ZERAR</a>
                 </div>
             </div>
         );
