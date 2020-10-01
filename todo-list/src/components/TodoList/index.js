02801e5766bba03369f9c0ce8a92dfcc03563e62
francovegini <francovegini@hotmail.com>
2020-09-30 22:44:01 -0300

    Implementado adicionar item

--- todo-list/src/components/TodoList/index.js
+++ todo-list/src/components/TodoList/index.js
@@ -5,17 +5,35 @@ class TodoList extends Component {
     constructor() {
         super();
         this.state = {
-            tarefa: ''
+            tarefa: '',
+            items: []
         }
+
+        this.addItem = this.addItem.bind(this);
+    }
+
+    addItem(e) {
+        let state = this.state;
+        if(this._tarefaInput.value !== ''){
+            let newItem = {
+                text: this._tarefaInput.value,
+                key: Date.now()
+            }
+            this.setState({ items: [...state.items, newItem]});
+        }
+
+        e.preventDefault();
     }
 
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={ this.addItem }>
                     <input type="text" placeholder="Nova tarefa" name="tarefa"
                            value={ this.state.tarefa }
-                           onChange={ (e) => this.setState({ tarefa: e.target.value }) }/>
+                           onChange={ (e) => this.setState({ tarefa: e.target.value }) }
+                           ref={ (e) => this._tarefaInput = e }/>
+                    <button type="submit">Adicionar tarefa</button>
                 </form>
 
                 {this.state.tarefa}
