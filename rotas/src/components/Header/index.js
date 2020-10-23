fe4c824d17a4101a5e16018658727890d46c4da8
francovegini <francovegini@hotmail.com>
2020-10-23 01:08:17 -0300

    Adicionando o Header nas rotas

--- rotas/src/components/Header/index.js
+++ rotas/src/components/Header/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 class Header extends Component {
     render() {
         return (
             <div>
                 Header Meu projeto
+                <br/>
+                <Link to="/sobre">Ir para Sobre</Link>
                 <hr/>
             </div>
         );
--- rotas/src/router.js
+++ rotas/src/router.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
+import Header from "./components/Header";
 
 const Routes = () => {
     return (
         <BrowserRouter>
+            <Header/>
             <Switch>
                 <Route exact path="/" component={ Home }/>
                 <Route exact path="/sobre" component={ Sobre }/>
