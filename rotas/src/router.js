9c94a382ade7e2486fb3a43a8d54d72873f4165f
francovegini <francovegini@hotmail.com>
2020-10-23 00:58:48 -0300

    Comentario switch

--- rotas/src/router.js
+++ rotas/src/router.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
 
 const Routes = () => {
     return (
         <BrowserRouter>
-            <Route exact path="/" component={ Home }/>
-            <Route exact path="/sobre" component={ Sobre }/>
+            <!-- Switch faz com que apenas um componente seja carregado ao mesmo tempo -->
+            <Switch>
+                <Route exact path="/" component={ Home }/>
+                <Route exact path="/sobre" component={ Sobre }/>
+            </Switch>
         </BrowserRouter>
     );
 }
