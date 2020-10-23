7f004885069a830b20888df583fb5c0da2f6adb9
francovegini <francovegini@hotmail.com>
2020-10-23 00:44:58 -0300

    Criado componente header e página home

--- rotas/src/App.js
+++ rotas/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Header from "./components/Header";
+import Home from "./pages/Home";
 
 class App extends Component {
   render() {
     return (
         <div>
-            <h1>ate</h1>
+            <Header/>
+            <Home/>
         </div>
     );
   }
---
+++ rotas/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                Header Meu projeto
+                <hr/>
+            </div>
+        );
+    }
+}
+
+export default Header;
