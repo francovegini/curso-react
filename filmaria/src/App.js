806064966fa57c58ac12e54e2a316e89d3c21921
francovegini <francovegini@hotmail.com>
2020-11-03 22:36:24 -0300

    Ajustes

--- filmaria/src/App.js
+++ filmaria/src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import React, { Component } from 'react';
+import './style.css';
 
-function App() {
-  return (
-      <div>
-
-      </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="app">
+                Teste
+            </div>
+        );
+    }
 }
 
 export default App;
