914498b908344a558ca16375b60b4e331a1a78f6
francovegini <francovegini@hotmail.com>
2020-11-03 22:48:54 -0300

    Criado routes e component Home

--- filmaria/src/App.js
+++ filmaria/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './style.css';
+import Routes from "./routes";
 
 class App extends Component {
     render() {
         return (
             <div className="app">
-                Teste
+                <Routes/>
             </div>
         );
     }
