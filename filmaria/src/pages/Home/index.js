9b1c6eb0835e874921f2ffe6d060a60bde6cf4e1
francovegini <francovegini@hotmail.com>
2020-11-06 22:14:32 -0300

    Realizado request para api de filmes

--- filmaria/src/pages/Home/index.js
+++ filmaria/src/pages/Home/index.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react'
 
-class Home extends Component{
+class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            filmes: []
+        };
+
+        this.loadFilmes = this.loadFilmes.bind(this);
+    }
+
+    componentDidMount() {
+        this.loadFilmes();
+    }
+
+    loadFilmes() {
+        let url = "https://sujeitoprogramador.com/r-api/?api=filmes"
+        fetch(url)
+            .then((r) => r.json())
+            .then((json) => {
+                this.setState({ filmes: json });
+                console.log(json);
+            })
+    }
+
     render() {
-        return(
+        return (
             <div>
                 Págima Home
             </div>
