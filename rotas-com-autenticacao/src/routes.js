966ffb7160386e2196c5738fd61239b076199e00
francovegini <francovegini@hotmail.com>
2020-11-15 23:47:52 -0300

    Feito redirec quando não estiver autenticado

--- rotas-com-autenticacao/src/routes.js
+++ rotas-com-autenticacao/src/routes.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Home from "./pages/Home";
+import Painel from "./pages/Painel";
+import { autenticado } from './auth';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route { ...rest } render={ props => (
+        autenticado() ? (
+            <Component { ...props } />
+        ) : (
+            <Redirect to={ { pathname: '/', state: { from: props.location } } }/>
+        )
+    ) }/>
+)
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={ Home }/>
+                <PrivateRoute exact path="/painel" component={ Painel }/>
+
             </Switch>
         </BrowserRouter>
     );
