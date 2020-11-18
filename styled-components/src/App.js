7f8b49a06b30356754833b7ebd21332968690d6d
francovegini <francovegini@hotmail.com>
2020-11-17 23:20:00 -0300

    estilização com css

--- styled-components/src/App.js
+++ styled-components/src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-        <div>
+        <div className="container">
+            <header className="header">
+                <a className="titulo">Projeto Styled</a>
+            </header>
+
+            <h1>Bem vindo ao sistema!</h1>
         </div>
     );
   }
