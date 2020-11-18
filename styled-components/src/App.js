5e5f6d65c238690794e3886ee5160c4ee956ceb7
francovegini <francovegini@hotmail.com>
2020-11-17 23:38:19 -0300

    Usando styled-components

--- styled-components/src/App.js
+++ styled-components/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import "./App.css";
+import { Container, Head, Titulo } from "./styles";
 
 class App extends Component {
   render() {
     return (
-        <div>
-        </div>
+        <Container>
+            <Head>
+                <Titulo>Projeto Styled</Titulo>
+            </Head>
+        </Container>
     );
   }
 }
---
+++ styled-components/src/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+display: flex;
+flex-direction: column;
+width: 100%;
+height: 500px;
+`;
+
+export const Head = styled.header`
+width: 100%;
+height: 70px;
+display: flex;
+background-color: brown;
+justify-content: center;
+align-items: center;
+`;
+
+export const Titulo = styled.a`
+font-size: 35px;
+color: #FFF;
+`;
