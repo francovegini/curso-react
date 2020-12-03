31aa254db5a76a19bf5782161442a9effd8e0cad
francovegini <francovegini@hotmail.com>
2020-12-03 19:36:40 -0300

    Criado componentes

---
+++ projeto-spa/src/components/Contato/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contato() {
+    return (
+        <div>
+            <h1>Entre em contato</h1>
+            <h2>Telefone: (47) 3333-22222</h2>
+            <h3>Rua Cabral, 123</h3>
+        </div>
+    );
+}
+
+export default Contato;
---
+++ projeto-spa/src/components/Inicio/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Inicio() {
+    return (
+        <div>
+            <h1>Bem-vindo a Home</h1>
+            <h2>Aprendendo SPA na prática com React</h2>
+            <h3>Evoluindo a cada dia!</h3>
+        </div>
+    );
+}
+
+export default Inicio;
