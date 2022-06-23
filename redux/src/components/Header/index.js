be1fd76b753da8caa612980193a0964933feabdc
Franco Vegini <francovegini@hotmail.com>
2022-06-23 01:54:47 -0300

    basic styling

--- redux/src/components/Header/index.js
+++ redux/src/components/Header/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+import "./style.css";
 
 function App() {
   return (
-    <header>
-      <h1>Header...</h1>
+    <header className="container">
+      <Link to="/">
+        <img className="logo" src={logo} alt="Logo do Projeto" />
+      </Link>
+
+      <Link className="reserva" to="/reservas">
+        <div>
+          <strong>Minhas reservas</strong>
+          <span>3 reservas</span>
+        </div>
+      </Link>
     </header>
   );
 }
