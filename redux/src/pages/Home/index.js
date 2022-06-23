130915c301ce03e5115d66e23083859c8a8f5a78
Franco Vegini <francovegini@hotmail.com>
2022-06-23 02:09:19 -0300

    called api

--- redux/src/pages/Home/index.js
+++ redux/src/pages/Home/index.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import api from "../../services/api";
 
 function Home() {
+
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    async function loadApi() {
+      const response = await api.get("trips");
+      setTrips(response.data);
+
+      console.log(response.data);
+    }
+
+    loadApi();
+  }, [])
+
   return (
     <div>
       <h1>Home</h1>
