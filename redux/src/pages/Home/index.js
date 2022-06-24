d4720b1617f1868dbecf5c80a3117b6a5e206306
Franco Vegini <francovegini@hotmail.com>
2022-06-24 00:10:13 -0300

    estilização home

--- redux/src/pages/Home/index.js
+++ redux/src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import api from "../../services/api";
+import {MdFlightTakeoff} from 'react-icons/md'
+import "./style.css";
 
 function Home() {
 
@@ -9,16 +11,32 @@ function Home() {
     async function loadApi() {
       const response = await api.get("trips");
       setTrips(response.data);
-
-      console.log(response.data);
     }
 
     loadApi();
-  }, [])
+  }, []);
 
   return (
     <div>
-      <h1>Home</h1>
+      <div className="box">
+        {trips.map(trip => (
+          <li key={trip.id}>
+            <img src={trip.image} alt={trip.title} />
+            <strong>{trip.title}</strong>
+            <span> Status: {trip.status ? 'Disponível' : 'Indisponível'}</span>
+
+            <button
+              type="button"
+              onClick={() => {}}
+            >
+              <div>
+                <MdFlightTakeoff size={16} color="#FFF" />
+              </div>
+              <span>SOLICITAR RESERVA</span>
+            </button>
+          </li>
+        ))}
+      </div>
     </div>
   );
 }
